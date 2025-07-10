7e946ad9a58c8385b2ed61da0fd4dd12fccef916
1102Redwolf <svkk5432109876@gmail.com>
2025-07-10 23:03:24 +0530

    Add files via upload

---
+++ arithmeticoperators.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x = 7,y = -5;
+    cout<<x-y<<endl;
+    cout<<x+y<<endl;
+    cout<<x*y<<endl;
+    cout<<x/y<<endl; // consider number which is before decimal
+}
